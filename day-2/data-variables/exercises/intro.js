10200626c266fd2c46d6a8b2f5306b8e997a68b9
Liam Perkins <liam.perkins.1992@gmail.com>
2020-08-11 09:04:44 +0100

    challenge 2& 3

---
+++ day-2/data-variables/exercises/intro.js
@@ -0,0 +1,12 @@
+const firstName = "Liam";
+const LastName = "Perkins";
+const age = 27;
+const myEmail = "liam.perkins.1992@gmail.com";
+const hasPets = true;
+const DoB = 1992;
+
+const message = "Hello my name is " + firstName + " " + LastName + " and I'm " + age + " years old."
+console.log(message);
+
+const birthdayInfo = "My name is " + firstName + " " + LastName + " and I was born in " +  DoB;
+console.log(birthdayInfo);
